d807f0494b22e1759ba9537265a0d40c79ad2d7a
Cedric Guillemet <cedric.guillemet@gmail.com>
2022-05-23 17:27:10 +0200

    new shaders

---
+++ src/Shaders/Source/FreePages.cs
@@ -0,0 +1,20 @@
+#include "bgfx_compute.sh"
+#include "Allocation.sh"
+
+uniform vec4 groupMin;//, worldMax;
+
+NUM_THREADS(1, 1, 1)
+void main()
+{
+    ivec3 groupMini = ivec3(groupMin.xyz);
+
+    ivec3 coord = ivec3(gl_GlobalInvocationID.xyz);
+    
+    ivec3 addr = groupMini + coord;
+
+    ivec4 tag = imageLoad(s_worldToPageTags, addr);
+    if (tag.x == 0)
+    {
+        AllocatePage(addr, 1.);
+    }
+}
