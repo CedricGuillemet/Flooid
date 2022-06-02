072aa07d4c055fc42240451c59dadbc0d60e7120
Cedric Guillemet <cedric.guillemet@gmail.com>
2022-06-02 21:40:07 +0200

    render next level

--- src/Shaders/Source/Render.fs
+++ src/Shaders/Source/Render.fs
@@ -360,6 +360,11 @@ void main()
         float jacobi = SamplePage(v_texcoord0.xy * 0.5).x + 0.5;
         gl_FragColor = vec4(jacobi, jacobi, jacobi, 1.);
         localCoord = mod(v_texcoord0.xy * 0.5, 1./16.);
+    } else if (abs(debugDisplay.z - 9.) < 0.001)
+    {
+        float jacobi = SamplePage(v_texcoord0.xy * 0.5).x + 0.5;
+        gl_FragColor = vec4(jacobi, jacobi, jacobi, 1.);
+        localCoord = mod(v_texcoord0.xy * 0.5, 1./16.);
     }
 else
     if (tag.x > 0.)
